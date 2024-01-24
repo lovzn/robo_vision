7f8aba19b6caa0e7138bc754922152b894f29f1d
lovzn <3111232811@qq.com>
2024-01-24 09:13:00 +0800

    trajectory is added
    tips:1\ accurate camera calibration is significant
    2\urdf documents should be correct

---
+++ sb/build/armor_tracker/CMakeFiles/armor_tracker.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/armor_tracker.dir/src/extended_kalman_filter.cpp.o"
+  "CMakeFiles/armor_tracker.dir/src/extended_kalman_filter.cpp.o.d"
+  "CMakeFiles/armor_tracker.dir/src/tracker.cpp.o"
+  "CMakeFiles/armor_tracker.dir/src/tracker.cpp.o.d"
+  "CMakeFiles/armor_tracker.dir/src/tracker_node.cpp.o"
+  "CMakeFiles/armor_tracker.dir/src/tracker_node.cpp.o.d"
+  "CMakeFiles/armor_tracker.dir/src/trajectory.cpp.o"
+  "CMakeFiles/armor_tracker.dir/src/trajectory.cpp.o.d"
+  "libarmor_tracker.pdb"
+  "libarmor_tracker.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/armor_tracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sb/build/rm_vision_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,6 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/tars-go/sb/install/auto_aim_interfaces/share/auto_aim_interfaces/package.sh"
+. "/home/tars-go/sb/install/armor_detector/share/armor_detector/package.sh"
+. "/home/tars-go/sb/install/armor_tracker/share/armor_tracker/package.sh"
+. "/home/tars-go/sb/install/rm_serial_driver/share/rm_serial_driver/package.sh"
+. "/home/tars-go/sb/install/rm_auto_aim/share/rm_auto_aim/package.sh"
